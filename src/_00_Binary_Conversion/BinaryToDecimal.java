b97893ff461b6c644d214762f639de59de12c5a3
root <root@ac8703df761b>
2021-02-04 16:45:36 +0000

    ip binary to decimal

--- src/_00_Binary_Conversion/BinaryToDecimal.java
+++ src/_00_Binary_Conversion/BinaryToDecimal.java
@@ -1,44 +1,9 @@
 package _00_Binary_Conversion;
 
-import javax.swing.JOptionPane;
 
 public class BinaryToDecimal {
 	public static void main(String[] args) {
-		boolean numReasonable = false;
-		int numGiven = 0;
-		// Binary numbers in Java can be represented with the '0b' prefix.
-		// For example:
-		System.out.println(0b111);
-		//This prints 7, because 111 is 7 in binary.
-		
-		//EXAMPLE: Convert 0 1 0 1 1 0 from binary to decimal
-		String askForNum = JOptionPane.showInputDialog("please give me a number :)");
-		numGiven = Integer.parseInt(askForNum);
-		while(!numReasonable) {
-			if (askForNum == "" + numGiven) {
-				numReasonable = true;
-				JOptionPane.showMessageDialog(null, numGiven + "is a great number!");
-			} else {
-				numReasonable = false;
-				askForNum = JOptionPane.showInputDialog("please choose a number other than " + askForNum + " >:(");
-			}
-		}
-		boolean thiss = false;
-		int i = 1;
-		while(!thiss) {
-			if(i * i >= numGiven) {
-				thiss = true;
-			} else {
-			i++;
-			}
-		}
-		boolean remainNil = false;
-		int binaryNum = 0;
-		while(!remainNil) {
-			if(i * i % 2 == 0) {
-				
-			}
-		}
+bit
 //		To convert binary to decimal, use the following algorithm:
 //			1. Start from the right most digit of the binary number and assign that bit the value of 1.
 //		 	 	eg: 0	1	0	1	1	0
